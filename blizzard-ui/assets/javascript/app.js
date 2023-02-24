d167bae5436a4f135b5df81f9b5bd8920cac6a7a
Spiros Makaronas <spmakaronas@gmail.com>
2023-02-24 10:10:53 +0200

    latest

--- blizzard-ui/assets/javascript/app.js
+++ blizzard-ui/assets/javascript/app.js
@@ -1,12 +1,16 @@
 window.onload = () => {
 
   window.gamesLoader = () => {
-    let gamesNumber = Math.floor(Math.random() * (7 - 1 + 1)) + 1;
-    let games = document.querySelectorAll(".games__games-wrapper a")
-
-    for (i = 0; i < gamesNumber; i++) {
-      games[i].classList.remove('hidden')
-    }
+    setTimeout(function(){
+      let gamesNumber = Math.floor(Math.random() * (7 - 1 + 1)) + 1;
+      let games = document.querySelectorAll(".games__games-wrapper a")
+  
+      for (i = 0; i < gamesNumber; i++) {
+        // Logic is to also place a loading icon on markup and remove the icon when you remove the hidden class from games
+        // If games were parsed through API call we would take a different approach to render is game along with animations
+        games[i].classList.remove('hidden')
+      }
+    }, 900)
   }
 
   gamesLoader()
